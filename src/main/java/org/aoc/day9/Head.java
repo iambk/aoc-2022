3e3d4e137754a3c2e2f03edab1dbe008f54bb037
Bharath Kumar <iambkofficial@gmail.com>
2022-12-09 14:29:30 +0530

    day 9

---
+++ src/main/java/org/aoc/day9/Head.java
@@ -0,0 +1,36 @@
+package org.aoc.day9;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Head implements Knot {
+    private int xPosition;
+    private int yPosition;
+
+    public Head() {
+        this.xPosition = 0;
+        this.yPosition = 0;
+    }
+
+    @Override
+    public void moveUp() {
+        this.xPosition--;
+    }
+
+    @Override
+    public void moveDown() {
+        this.xPosition++;
+    }
+
+    @Override
+    public void moveLeft() {
+        this.yPosition--;
+    }
+
+    @Override
+    public void moveRight() {
+        this.yPosition++;
+    }
+}
